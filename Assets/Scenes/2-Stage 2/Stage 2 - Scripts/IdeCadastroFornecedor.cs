f7df65e308738ac36e3feee3fdb40824028bc31a
guilhermescosta <guilhermescosta@gmail.com>
2021-02-17 15:46:30 -0300

    atualizacoes fase 3 e 4

---
+++ Assets/Scenes/2-Stage 2/Stage 2 - Scripts/IdeCadastroFornecedor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class IdeCadastroFornecedor : MonoBehaviour
+{
+    public InputField var1;
+    int err1;
+
+    public InputField command1;
+    int err2;
+
+    
+
+    //   public GameObject commandIde;
+
+    public void CreateText()
+    {
+        string path = Application.dataPath + "/resultado.txt";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Resultado \nFase 2 erro1: " + err1 + " erro2: " + err2);
+        }
+        else
+        {
+            string content = "\nFase 2 erro1: " + err1 + " erro2: " + err2;
+            File.AppendAllText(path, content);
+        }
+
+    }
+
+    public void PlayButton()
+    {
+        if (string.Compare(var1.text, "scanf(\"%d\",&cnpj);") != 0)
+        {
+            Debug.Log("erro");
+            err1++;
+        }
+        else if (string.Compare(command1.text, "scanf(\"%s\",email);") != 0)
+        {
+            Debug.Log("erro 2");
+            err2++;
+        }
+
+
+        else
+        {
+            Debug.Log("Tudo Ok");
+            // commandIde.SetActive(true);
+            CreateText();
+
+        }
+
+    }
+}
---
+++ Assets/Scenes/4-Stage 4/Stage 4 - Scripts/IdeMenuPrincipal.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class IdeMenuPrincipal : MonoBehaviour
+{
+    public InputField varSwitch;
+    int err1;
+    public InputField varCase;
+    int err2;
+
+    public InputField varDefault;
+    int err3;
+
+    public InputField var3;
+    int err4;
+
+    public InputField var4;
+    int err5;
+
+//    public GameObject commandIde;
+
+    //  relatorio
+
+
+
+    public void CreateText()
+    {
+        string path = Application.dataPath + "/resultado.txt";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Resultado \nFase 4 erro1: " + err1 + " erro2: " + err2 + " erro3: " + err3);
+        }
+        else
+        {
+            string content = "Resultado \nFase 1 erro4: " + err1 + " erro2: " + err2 + " erro3: " + err3;
+            File.AppendAllText(path, content);
+        }
+    }
+
+    private void Start()
+    {
+        CreateText();
+    }
+
+    public void CloseCommand()
+    {
+       // commandIde.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        // Debug.Log(string.Compare(var1.text, "validaSenha"));
+        if (string.Compare(varSwitch.text, "switch(valor)") != 0)
+        {
+            Debug.Log("erro");
+            err1++;
+        }
+        else if (string.Compare(varCase.text, "case" + " "+ "4 :" ) != 0)
+        {
+            Debug.Log("erro 2");
+            err2++;
+        }
+
+        // linha 2
+        else if (string.Compare(varDefault.text, "default:") != 0)
+        {
+            Debug.Log("erro 3");
+            err3++;
+        }
+
+
+        else
+        {
+            Debug.Log("Tudo Ok");
+          //  commandIde.SetActive(true);
+            CreateText();
+
+        }
+
+    }
+}
+
