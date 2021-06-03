5fe2cd0ec75ed2300ada9f6171b59394f4d3e80d
guilhermescosta <guilhermescosta@gmail.com>
2021-06-03 11:41:05 -0300

    atualizacao fases 1-1, 1-2 e 1-3

---
+++ Assets/Scenes/1-2.Stage 1/1-2-Scripts/IdeSystemLoginScanf.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class IdeSystemLoginScanf : MonoBehaviour
+{
+    public InputField var1;
+    int err1;
+
+    public InputField var2;
+    int err2;
+
+    public InputField var3;
+    int err3;
+
+    public InputField var4;
+    int err4;
+
+    public Text erros;
+
+    public GameObject telaFinal;
+
+
+    public void CreateText()
+    {
+        string path = Application.dataPath + "/resultado.txt";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "\nFase 1-2 erro1: " + err1 + " erro2: " + err2 + " erro3: " + err3 + " erro4: " + err4);
+        }
+        else
+        {
+            string content = "\nFase 1-2 erro1: " + err1 + " erro2: " + err2 + " erro3: " + err3 + " erro4: " + err4;
+            File.AppendAllText(path, content);
+        }
+    }
+
+    public void Continuar()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+
+        if (string.Compare(var4.text, "scanf(\"%d\",&senha);") != 0)
+        {
+            err4++;
+            erros.text = "Erro na linha leitura variavel senha  ";
+            Debug.Log("scanf(\"%d\",&senha);");
+        }
+
+        if (string.Compare(var3.text, "scanf(\"%s\",&usuario);") != 0)
+        {
+            err3++;
+            erros.text = "Erro na linha leitura variavel usuario  ";
+            Debug.Log("scanf(\"%s\",&usuario);");
+        }
+
+        if (string.Compare(var2.text, "int senha=0;") != 0)
+        {
+            err2++;
+            erros.text = "Erro na linha definicao variavel senha ";
+        }
+
+        if (string.Compare(var1.text, "char usuario[20];") != 0)
+        {
+            err1++;
+            erros.text = "Erro na linha definicao variavel usuario  ";
+        }
+
+        if (string.Compare(var1.text, "char usuario[20];") != 0 || string.Compare(var2.text, "int senha=0;") != 0 || string.Compare(var3.text, "scanf(\"%s\",&usuario);") != 0 || string.Compare(var4.text, "scanf(\"%d\",&senha);") != 0)
+        {
+            Debug.Log(var4);
+
+            Debug.Log("erro");
+        }
+        else
+        {
+            //  Debug.Log("Tudo Ok");
+            erros.text = "Arquivo compilado com sucesso ";
+            // commandIde.SetActive(true);
+            CreateText();
+            telaFinal.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/Scenes/1-2.Stage 1/1-3-Scripts/ConsoleSystemLogin.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConsoleSystemLogin : MonoBehaviour
+{
+    public InputField user;
+    public InputField pass;
+    public Text output;
+
+    public Lesson _lesson;
+    public GameObject telaFinal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fim()
+    {
+        telaFinal.SetActive(true);
+    }
+
+    public void Encerrar()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Result()
+    {
+        if (string.Compare(user.text, "haskell") != 0)
+        {
+            output.text = "usuário não cadastrado\n";
+        }
+        else if (string.Compare(pass.text, "senha123") != 0)
+        {
+            output.text = "senha incorreta";
+        }
+        else
+        {
+            output.text = "Usuário Logado\nTeste Encerrado, reportar Haskell";
+            Fim();
+            //_lesson.aula = 2;
+        }
+    }
+
+
+}
